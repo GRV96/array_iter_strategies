e1e275c71d5c47c819703277b7b35870633f55c6
Guyllaume Rousseau <guyllaume.rousseau.1@ens.etsmtl.ca>
2021-05-29 16:18:09 -0400

    Test with and without a length variable

---
+++ src/LengthVariableStrategy.java
@@ -0,0 +1,46 @@
+
+public final class LengthVariableStrategy {
+
+	private static final int ITERATION_COUNT = 100;
+
+	private static int arraySumNoLengthVar(int[] array) {
+		int sum = 0;
+
+		for(int i=0; i<array.length; i++) {
+			sum += array[i];
+		}
+
+		return sum;
+	}
+
+	private static int arraySumWithLengthVar(int[] array) {
+		int sum = 0;
+		int length = array.length;
+
+		for(int i=0; i<length; i++) {
+			sum += array[i];
+		}
+
+		return sum;
+	}
+
+	public static void main(String[] args) {
+		int[] testArray = IntArrays.makeRandIntArray(100, 0, 100);
+
+		long startTime = System.nanoTime();
+		for(int i=0; i<ITERATION_COUNT; i++) {
+			arraySumNoLengthVar(testArray);
+		}
+		long endTime = System.nanoTime();
+		System.out.println("Execution time with no length variable: "
+				+ (double) (endTime-startTime)/1000 + " microsec");
+
+		startTime = System.nanoTime();
+		for(int i=0; i<ITERATION_COUNT; i++) {
+			arraySumWithLengthVar(testArray);
+		}
+		endTime = System.nanoTime();
+		System.out.println("Execution time with length variable: "
+				+ (double) (endTime-startTime)/1000 + " microsec");
+	}
+}
